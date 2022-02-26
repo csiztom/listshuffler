ff86797014d2e39df9c6cd2c032dbe34af94204b
csiztom <tom.csiz@gmail.com>
2022-02-26 18:51:11 +0100

    fix fe-test

--- listshuffler-fe/src/pages/Start.tsx
+++ listshuffler-fe/src/pages/Start.tsx
@@ -1,4 +1,4 @@
-import { Grid, GridItem, Heading, Button, Input } from '@chakra-ui/react'
+import { Grid, GridItem } from '@chakra-ui/react'
 import { ReactElement } from 'react'
 import { Logo } from '../components'
 import ActionCard from '../components/ActionCard'
