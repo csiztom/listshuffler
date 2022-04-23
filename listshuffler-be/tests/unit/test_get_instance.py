331f7ab630f14060324b04cc80cdb1c71f20c103
csiztom <tom.csiz@gmail.com>
2022-04-23 19:11:59 +0200

    fix

--- listshuffler-be/tests/unit/test_get_instance.py
+++ listshuffler-be/tests/unit/test_get_instance.py
@@ -1,4 +1,5 @@
 import json
+import datetime
 from unittest import TestCase, mock
 from src.get_instance import app
 
@@ -23,7 +24,7 @@ class TestGetInstance(TestCase):
     def test_emptyInstance(self, mock_pymysql):
         mock_cursor = mock.MagicMock()
         mock_cursor.fetchall.return_value = []
-        mock_cursor.fetchone.return_value = ['id', 0, 'id2', True, None, '2000-10-26']
+        mock_cursor.fetchone.return_value = ['id', 0, 'id2', True, None, datetime.datetime(2020, 5, 17)]
         mock_pymysql.connect.return_value.cursor.return_value.__enter__.return_value = mock_cursor
         res = app.handler(apigw_event(), "")
 
@@ -37,7 +38,7 @@ class TestGetInstance(TestCase):
     def test_oneListInstance(self, mock_pymysql):
         mock_cursor = mock.MagicMock()
         mock_cursor.fetchall.return_value = [['id', 'name', 1]]
-        mock_cursor.fetchone.return_value = ['id', 0, 'id2', True, None, '2000-10-26']
+        mock_cursor.fetchone.return_value = ['id', 0, 'id2', True, None, datetime.datetime(2020, 5, 17)]
         mock_pymysql.connect.return_value.cursor.return_value.__enter__.return_value = mock_cursor
         res = app.handler(apigw_event(), "")
 
@@ -48,7 +49,7 @@ class TestGetInstance(TestCase):
     def test_moreListInstance(self, mock_pymysql):
         mock_cursor = mock.MagicMock()
         mock_cursor.fetchall.return_value = [['id', 'name', 1], ['id2', 'name2', 1]]
-        mock_cursor.fetchone.return_value = ['id', 0, 'id2', True, None, '2000-10-26']
+        mock_cursor.fetchone.return_value = ['id', 0, 'id2', True, None, datetime.datetime(2020, 5, 17)]
         mock_pymysql.connect.return_value.cursor.return_value.__enter__.return_value = mock_cursor
         res = app.handler(apigw_event(), "")
 
